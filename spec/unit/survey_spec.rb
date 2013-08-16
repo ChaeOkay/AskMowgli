b862314f3c54c4092516d67083c5d70c96f24f92
cokeefe <chaemobile@gmail.com>
2013-08-16 11:51:09 -0700

    survey specs

--- spec/unit/survey_spec.rb
+++ spec/unit/survey_spec.rb
@@ -1,1 +1,18 @@
 require 'spec_helper'
+
+describe Survey do
+
+  describe "a new survey" do
+    it "should be valid" do
+      survey = Survey.create(name: 'survey1')
+      survey.should be_valid
+      survey.delete
+    end
+
+    it "should not be valid without a name" do
+      survey = Survey.create()
+      survey.should_not be_valid
+      survey.delete
+    end
+  end
+end
