cd030efad57e9f5b7ead3dbdaca902dd7fff4bc7
Seth Hochberg <seth@sethhochberg.com>
2013-08-16 14:24:23 -0700

    fix typos in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ survey1 = Survey.create(name: "Condiments", user: a)
 survey2 = Survey.create(name: "Woodland Creatures", user: b)
 survey3 = Survey.create(name: "Rocks", user: c)
 
-question1= Question.create(survey_type: 'text', descripton: 'For use on burgers or sandwiches; for instance, mayo, mustard, relish, ketchup, etc...', prompt: 'Favorite condiment?', survey: survey1)
-question2= Question.create(survey_type: 'text', descripton: 'For combat? For instance, perhaps the mighty wolverine...', prompt: 'Which woodland creature is best?', survey: survey2)
-question3= Question.create(survey_type: 'text', descripton: 'Start with type, such as igneous, sedimentary, or metamorphic. Need not be of terrestrial origin, either (i.e. moon rocks, meteorites, etc.', prompt: 'What is your favorite rock?', survey: survey3)
+question1= Question.create(question_type: 'text', description: 'For use on burgers or sandwiches; for instance, mayo, mustard, relish, ketchup, etc...', prompt: 'Favorite condiment?', survey: survey1)
+question2= Question.create(question_type: 'text', description: 'For combat? For instance, perhaps the mighty wolverine...', prompt: 'Which woodland creature is best?', survey: survey2)
+question3= Question.create(question_type: 'text', description: 'Start with type, such as igneous, sedimentary, or metamorphic. Need not be of terrestrial origin, either (i.e. moon rocks, meteorites, etc.', prompt: 'What is your favorite rock?', survey: survey3)
 
