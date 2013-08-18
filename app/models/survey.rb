2de1f7f0f43466dcb874cd5115d3c4898267bc36
cokeefe <chaemobile@gmail.com>
2013-08-18 13:25:22 -0700

    update model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,6 +5,28 @@ class Survey < ActiveRecord::Base
   validates_presence_of :name
 
   def stats
+    @stats = []
+
+    # a survey has many questions
+      # a reply belongs to a queston and a user_id
+          # a reply can be a response_choice
+
+  # how many users have started the survey?
+    # survey.questions.user_id.unique.count
+    questions.where(users: {})
+    #> Survey.joins(questions: :replies)
+
+  # how many of those users have completed the survey?
+    # show user_Id from survey questions where count(user_id) == number of questions
+     # survey.questions.user_id where
+      # user_id count is == number of questions
+
+      # can a there be multiple replys where user_id and question_id are the same?
+
+  # questions each
+    # show question
+    # show response value and # of people with same response
+      # question.values.each => hash[value] += 1
   end
 
 end
